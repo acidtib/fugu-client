728fabb7057eb88adb7280066676784cf3f3ce56
Dan Vera <dan@yovu.co>
2013-06-19 10:37:02 -0400

    check if im online

--- Resources/app.js
+++ Resources/app.js
@@ -1,18 +1,8 @@
-//Open the database first
-var db = Ti.Database.openFile(Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'fugu.db'));   
+var app = Ti.API.application;
+//alert(app.getDataPath());
 
-//Create a table and insert values into it
-db.execute("CREATE TABLE IF NOT EXISTS Users(id INTEGER, firstName TEXT)");
-db.execute("INSERT INTO Users VALUES(1,'Joe Bloggs')");      
-
-//Select from Table
-var rows = db.execute("SELECT * FROM Users");
-while (rows.isValidRow()) {
-    //Alert the value of fields id and firstName from the Users database
-    alert('The user id is '+rows.fieldByName('id')+', and user name is '+rows.fieldByName('firstName'));
-    rows.next();    
-}
-
-//Release memory once you are done with the resultset and the database
-rows.close();
-db.close();
+if (navigator.onLine) {
+  	alert('Im Online');
+} else {
+	alert('Im Not Online');
+} 
