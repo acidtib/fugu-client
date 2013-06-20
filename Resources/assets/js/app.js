588e2156a71cac4cbe7525dac982d47708cd9512
Dan Vera <dan@yovu.co>
2013-06-19 22:47:55 -0400

    a few cimments

--- Resources/assets/js/app.js
+++ Resources/assets/js/app.js
@@ -1,21 +1,26 @@
+//Check if theres internet connection
 if (navigator.onLine) {
   	
   	$(function() {
+  		//Validate the form to make sure it has content
 		$(".initial_keys").validate({
    			submitHandler: function(form) {
      			
+     			//Grab the values from the form
      			client = $('input[id=client]').val();
      			api = $('input[id=api]').val();
      			//alert(client + api);
 
      			var success = false;
 
+     			//Make a request to validate that the values work
      			jQuery.getJSON("https://api.digitalocean.com/droplets/?client_id="+ client +"&api_key="+api, function(json) {
     				success = true;
 
     				alert(json['status']);
 				});
 
+     			//If they dont work then freak out
 				setTimeout(function() {
 				    if (!success)
 				    {
@@ -35,7 +40,6 @@ if (navigator.onLine) {
 
 	//Create the table
 	db.execute("CREATE TABLE IF NOT EXISTS API(id INTEGER, client TEXT, key TEXT)");
-
 
 	//Lets close it up
 	db.close();
